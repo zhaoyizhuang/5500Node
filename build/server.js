8420beb0665a0f9d94737cb94547e3a9308bee0a
ericzzy <ericzhuangzhaoyi@outlook.com>
2022-01-22 16:56:47 -0500

    Node Generate

---
+++ build/server.js
@@ -0,0 +1,13 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.get('/hello', (req, res) => res.send('Hello World!'));
+app.get('/add/:a/:b', (req, res) => {
+    res.send(req.params.a + req.params.b);
+});
+const PORT = 4000;
+app.listen(process.env.PORT || PORT);
