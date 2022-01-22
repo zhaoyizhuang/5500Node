53689b0c8524faebec9ca1327bb8d39b9391612e
ericzzy <ericzhuangzhaoyi@outlook.com>
2022-01-22 17:55:44 -0500

    heroku

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 4000;
 
 app.get('/hello', (req, res) =>
   res.send('Hello World!'));
 
-const PORT = 4000;
-app.listen(process.env.PORT || PORT);
+//const PORT = 4000;
+app.listen(PORT);
 //app.listen(PORT);
--- server.ts
+++ server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 const app = express();
+const PORT = process.env.PORT || 4000;
 
 app.get('/hello', (req, res) =>
     res.send('Hello World!'));
@@ -8,5 +9,5 @@ app.get('/add/:a/:b', (req, res) => {
     res.send(req.params.a + req.params.b);
 })
 
-const PORT = 4000;
-app.listen(process.env.PORT || PORT);
+//const PORT = 4000;
+app.listen(PORT);
