9f391878dbcd5a2c340596ee88d002ac274de18b
ericzzy <ericzhuangzhaoyi@outlook.com>
2022-03-21 17:33:57 -0400

    change origin cors

--- server.ts
+++ server.ts
@@ -37,7 +37,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
     credentials: true,
-    origin: 'https://jazzy-bavarois-13510f.netlify.app'
+    origin: 'https://zzy5500.netlify.app'
     // origin: 'http://localhost:3000'
 }));
 
