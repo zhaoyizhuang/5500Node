f6c8aa183233ea4511b6bd325162bac56989f577
ericzzy <ericzhuangzhaoyi@outlook.com>
2022-03-21 16:43:27 -0400

    JSDoc update

--- controllers/AuthController.ts
+++ controllers/AuthController.ts
@@ -3,10 +3,31 @@ import UserDao from "../daos/UserDao";
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
+/**
+ * @class AuthenticationController Implements RESTful Web service API for authentication resource.
+ * Defines the following HTTP endpoints:
+ * <ul>
+ *     <li> POST /auth/login to log in the user.
+ *     </li>
+ *     <li> POST /auth/register to register a new user
+ *     </li>
+ *     <li> POST /auth/profile to retrieve the profile of the user
+ *     </li>
+ *     <li> POST /auth/logout  to logout the user.
+ *     </li>
+ * </ul>
+ * @property {UserDao} userDao Singleton DAO implementing user CRUD operations
+ * RESTful Web service API
+ */
 const AuthenticationController = (app: Express) => {
 
     const userDao: UserDao = UserDao.getInstance();
 
+    /**
+     * Log in the user
+     * @param req request with username and password
+     * @param res response with logged in user
+     */
     const login = async (req: Request, res: Response) => {
         const user = req.body;
         const username = user.username;
@@ -33,6 +54,11 @@ const AuthenticationController = (app: Express) => {
         }
     }
 
+    /**
+     * register the user
+     * @param req request with username, password and email
+     * @param res response with created user
+     */
     const register = async (req: Request, res: Response) => {
         const newUser = req.body;
         const password = newUser.password;
@@ -54,6 +80,11 @@ const AuthenticationController = (app: Express) => {
         }
     }
 
+    /**
+     * user profile
+     * @param req request with session
+     * @param res response with previous saved profile
+     */
     const profile = (req: Request, res: Response) => {
         // @ts-ignore
         const profile = req.session['profile'];
@@ -64,12 +95,20 @@ const AuthenticationController = (app: Express) => {
         }
     }
 
+    /**
+     * log out the user.
+     * @param req request with session
+     * @param res response with status.
+     */
     const logout = (req: Request, res: Response) => {
         // @ts-ignore
         req.session.destroy();
         res.sendStatus(200);
     }
 
+    /**
+     * Restful Web Service API
+     */
     app.post("/auth/login", login);
     app.post("/auth/register", register);
     app.post("/auth/profile", profile);
